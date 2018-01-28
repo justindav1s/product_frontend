04c9fd481784f6e20491c84bacbcbd973ae49e72
Justin Davis <justinndavis@gmail.com>
2018-01-28 14:38:40 +0000

    Inventory Manager, client for product service

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ http = urllib3.PoolManager()
 @app.route('/products', methods=['GET', 'POST'])
 def index():
     form = ProductForm()
-    r = http.request('GET', 'https://product:8080/product/1')
+    r = http.request('GET', 'http://product:8080/product/1')
     print(r.data)
     parsed_json = json.loads(r.data.decode("utf-8"))
     prod = Product(parsed_json['id'], parsed_json['name'])
