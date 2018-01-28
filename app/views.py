84e58d5ade4fc8dfdabb77b2580eb66bbbfb4922
Justin Davis <justinndavis@gmail.com>
2018-01-28 17:53:29 +0000

    Inventory Manager, client for product service

--- app/views.py
+++ app/views.py
@@ -15,9 +15,9 @@ def index():
 
     inventory = []
 
-    r = http.request('GET', 'http://productsrv:8080/product/')
+    #r = http.request('GET', 'http://productsrv:8080/product/')
     r = http.request('GET', 'http://productv3:8080/product/')
-    #r = http.request('GET', 'http://productv3-prod-services.172.16.173.128.nip.io/product/')
+    r = http.request('GET', 'http://productv3-prod-services.172.16.173.128.nip.io/product/')
     print(r.data)
 
     parsed_json = json.loads(r.data.decode("utf-8"))
